e73ee9c442f648ff24c723612a937329369489ce
weevood <5528482+weevood@users.noreply.github.com>
2020-11-11 15:45:16 +0100

    Moved

--- src/components/Stickies/index.js
+++
@@ -1,7 +0,0 @@
-import {default as Stickies} from './Stickies';
-
-export {
-	Stickies
-};
-
-export default Stickies;
--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-export const STATE_TODAY = 'today';
-
-export const TYPE_REMAINS = 'remains';
-export const TYPE_REPEAT = 'repeat';
-export const TYPE_ONCE = 'once';
-
-export const COLOR_REMAINS = '#c0392b';
-export const COLOR_REPEAT = '#2980b9';
-export const COLOR_ONCE = '#f1c40f';
